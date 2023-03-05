21cc4d5b8e4a6c5b68fb66b7f8ef007c5f0243c2
fadilnjimoun <104419651+fadilnjimoun@users.noreply.github.com>
2023-03-05 05:13:51 +0100

    Update streamlit_pybet.py

--- Streamlit/streamlit_pybet.py
+++ Streamlit/streamlit_pybet.py
@@ -453,12 +453,12 @@ le2 = LabelEncoder()
 
 le2.fit(data.loc[:,'player1'].append(data.loc[:,'player2'], ignore_index=True).drop_duplicates())
 
-X_train_cat_encod2 = pd.DataFrame({X_train_cat2.columns[0] : le1.fit_transform(X_train_cat2.iloc[:,0]),
-                                   X_train_cat2.columns[1] : le2.transform(X_train_cat2.iloc[:,1]),
-                                   X_train_cat2.columns[2] : le2.transform(X_train_cat2.iloc[:,2])}, index=X_train.index)
-X_test_cat_encod2 = pd.DataFrame({X_test_cat2.columns[0] : le1.fit_transform(X_test_cat2.iloc[:,0]),
-                                   X_test_cat2.columns[1] : le2.transform(X_test_cat2.iloc[:,1]),
-                                   X_test_cat2.columns[2] : le2.transform(X_test_cat2.iloc[:,2])}, index=X_test.index)
+X_train_cat_encod2 = pd.DataFrame({X_train_cat2.columns[0] : le1.fit_transform(X_train_cat2[X_train_cat2.columns[0]]),
+                                   X_train_cat2.columns[1] : le2.transform(X_train_cat2[X_train_cat2.columns[1]]),
+                                   X_train_cat2.columns[2] : le2.transform(X_train_cat2[X_train_cat2.columns[2]])}, index=X_train.index)
+X_test_cat_encod2 = pd.DataFrame({X_test_cat2.columns[0] : le1.fit_transform(X_test_cat2[X_test_cat2.columns[0]]),
+                                   X_test_cat2.columns[1] : le2.transform(X_test_cat2[X_test_cat2.columns[1]]),
+                                   X_test_cat2.columns[2] : le2.transform(X_test_cat2[X_test_cat2.columns[2]])}, index=X_test.index)
 
 X_train = pd.concat([X_train_cat_encod1, X_train_cat_encod2, X_train_num_scaled], axis=1)
 X_test = pd.concat([X_test_cat_encod1, X_test_cat_encod2, X_test_num_scaled], axis=1)""")
